5da8f421b347b878f6d095786dd27984453d8fd2
Josef Rabmer <josef.rabmer@student.tuwien.ac.at>
2024-02-19 15:23:29 +0100

    Added notice in case of error

--- src/converter.ts
+++ src/converter.ts
@@ -1,5 +1,5 @@
 import GitLabWikiConverterPlugin from 'main';
-import { FileSystemAdapter, TAbstractFile, TFolder, Vault } from 'obsidian';
+import { FileSystemAdapter, Notice, TAbstractFile, TFolder, Vault } from 'obsidian';
 import * as fs from 'fs/promises'
 import * as path from 'path';
 import { removeFileExtensionsForMdFiles } from 'fileExtensionStripper'
@@ -33,15 +33,27 @@ const exportVaultToSpecifiedLocation = async (plugin: GitLabWikiConverterPlugin)
     if (adapter instanceof FileSystemAdapter) {
         const vaultAbsolutePath = adapter.getBasePath();
         const exportPath = plugin.settings.exportPath.split(path.sep).join(path.posix.sep);
-
+        
         Vault.recurseChildren(plugin.app.vault.getRoot(), async (file: TAbstractFile) => {
             if (file instanceof TFolder && file.isRoot()) {
-                await fs.mkdir(exportPath);
+                try {
+                    await fs.mkdir(exportPath);    
+                } catch (error) {
+                    console.log("Export failed: Could not find or create export folder! Check path specified in settings.");
+                    new Notice("Export failed: Could not find or create export folder! Check path specified in settings.", 0);
+                    return;
+                }  
             } else {
-                if (file instanceof TFolder) {
-                    await fs.mkdir(path.posix.join(exportPath, file.path));
-                } else {
-                    await fs.copyFile(path.posix.join(vaultAbsolutePath, file.path), path.posix.join(exportPath, file.path));
+                try {
+                    if (file instanceof TFolder) {
+                        await fs.mkdir(path.posix.join(exportPath, file.path));
+                    } else {
+                        await fs.copyFile(path.posix.join(vaultAbsolutePath, file.path), path.posix.join(exportPath, file.path));
+                    }   
+                } catch (error) {
+                    console.log("Export failed: Could not write to export folder! Check that path is correct and you have the corresponding permissions.");
+                    new Notice("Export failed: Could not write to export folder! Check that path is correct and you have the corresponding permissions.", 0);
+                    return;
                 }
             }
         })
